68f92548ea85473bc3799e0acd90d9fb847518f2
Kayo Elias <contatodevkayo@gmail.com>
2023-08-25 13:11:57 -0300

    refactor: adapter unused return

--- src/pages/DashboardPage/pages/Users/ListAllUsers/adapters/usersFromApi.ts
+++ src/pages/DashboardPage/pages/Users/ListAllUsers/adapters/usersFromApi.ts
@@ -1,31 +1,8 @@
-export interface Geo {
-  lat: string
-  lng: string
-}
-
-export interface Company {
-  name: string
-  catchPhrase: string
-  bs: string
-}
-
-export interface Address {
-  street: string
-  suite: string
-  city: string
-  zipcode: string
-  geo: Geo
-}
-
 export interface UsersFromApi {
   id: number
   name: string
   username: string
   email: string
-  address: Address
-  phone: string
-  website: string
-  company: Company
 }
 
 export interface UsersMapped {
@@ -33,10 +10,6 @@ export interface UsersMapped {
   name: string
   username: string
   email: string
-  address: Address
-  phone: string
-  website: string
-  company: Company
 }
 export const usersFromApi = (data: UsersFromApi[]): UsersMapped[] => {
   return data.map((item) => {
@@ -45,10 +18,6 @@ export const usersFromApi = (data: UsersFromApi[]): UsersMapped[] => {
       name: item.name,
       username: item.username,
       email: item.email,
-      address: item.address,
-      phone: item.phone,
-      website: item.website,
-      company: item.company,
     }
   })
 }
