63d3b1477abea769f43abdc2a5ebe60ef47ab949
Kayo Elias <contatodevkayo@gmail.com>
2023-08-25 02:20:34 -0300

    feat: :sparkles: vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': '/src',
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: false,
+    coverage: {
+      provider: 'v8', // or 'v8'
     },
   },
 })
