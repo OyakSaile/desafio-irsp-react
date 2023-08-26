94b3069ec495c311153527768b1d9fd44e6b1fd9
Kayo Elias <contatodevkayo@gmail.com>
2023-08-26 17:50:37 -0300

    feature: components provider separated

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,12 @@ import './styles/index.css'
 import { router } from './routes/dashboardRoutes.tsx'
 import { RouterProvider } from 'react-router-dom'
 import { LoadingProvider } from './hooks/useLoading.tsx'
-import { LoadingSpin } from './components/LoadingSpin/index.tsx'
+import { ComponentsUsedsWithProviders } from './utils/ComponentsUsedsWithProviders.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <LoadingProvider>
-      <LoadingSpin />
+      <ComponentsUsedsWithProviders />
       <RouterProvider router={router} />
     </LoadingProvider>
   </React.StrictMode>,
