04afd22a6c671810dce31ff9d887b9a4a2b2eaeb
Kayo Elias <contatodevkayo@gmail.com>
2023-08-26 16:26:46 -0300

    build: :building_construction: storybook configuration, eslint configuration, package added

--- vite.config.ts
+++ vite.config.ts
@@ -8,15 +8,19 @@ import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
   test: {
     globals: true,
     environment: 'jsdom',
-    css: false,
+    setupFiles: './src/test/setup.ts',
     coverage: {
-      provider: 'v8', // or 'v8'
+      provider: 'v8',
     },
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
   },
 })
