c1b254ca878a77662df7162532114ab948586cba
Kayo Elias <contatodevkayo@gmail.com>
2023-08-26 23:58:46 -0300

    feature: toast container

--- src/utils/ComponentsUsedsWithProviders.tsx
+++ src/utils/ComponentsUsedsWithProviders.tsx
@@ -1,7 +1,14 @@
 import { LoadingSpin } from '@/components/LoadingSpin'
 import { useLoading } from '@/hooks/useLoading'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export const ComponentsUsedsWithProviders = () => {
   const { isLoading } = useLoading()
-  return <LoadingSpin isLoading={isLoading} />
+  return (
+    <>
+      <LoadingSpin isLoading={isLoading} />
+      <ToastContainer />
+    </>
+  )
 }
